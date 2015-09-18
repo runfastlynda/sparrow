ce0e8d31a3eee85ce36f992191065576cd24a2b2
runfastlynda <runfastlynda@gmail.com>
2015-09-18 17:59:37 +0800

    init project

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+    gulp.src('app/scss/main.scss')
+        .pipe(sass())   
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('browser-sync', ['sass'], function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+    })
+});
+gulp.task('watch', function () {
+    gulp.watch('app/scss/*.scss', ['sass']);
+    gulp.watch(['app/*.html', 'app/css/*.css', 'app/js/*.js', 'app/img/*'], browserSync.reload);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
